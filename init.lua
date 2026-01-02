4af7c6061adc5d1333ecce6ef9f6e71ed6900452
Andrew Matkovsky <me@amatkovsky.com>
2026-01-02 11:40:22 -0500

    Replace vim config with nvim config

---
+++ init.lua
@@ -0,0 +1,13 @@
+-- Key mappings
+vim.keymap.set('i', 'jk', '<ESC>', { noremap = true })
+
+-- Editor settings
+vim.opt.number = true
+vim.opt.swapfile = false
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.clipboard = 'unnamedplus'  -- System clipboard integration
+vim.opt.expandtab = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
